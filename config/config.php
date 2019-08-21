57ffa206910c07b77403bf145da25d0b0f494173
anjie <anjing.public@gmail.com>
2019-08-21 11:51:50 +0800

    composer.json 添加了psr-4

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@
 *  Geetest配置文件
 * @author Tanxu
 */
-define("CAPTCHA_ID", "48a6ebac4ebc6642d68c217fca33eb4d");
-define("PRIVATE_KEY", "4f1c085290bec5afdc54df73535fc361");
+define("CAPTCHA_ID", "40e3b4fde96600cf0ed143ea50120e06");
+define("PRIVATE_KEY", "48c5b9764dc632f7a08d7aca68bcb781");
 
 
 
